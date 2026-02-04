97962cec97e57b389011bb59d17e5e88e13bee43
ABRAR ALi <abraraliengineer@gmail.com>
2026-02-04 12:05:59 +0500

    order Success

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,16 +1,18 @@
  import React from 'react'
 // import Cart_page from './components/Cart_page'
-import Checkout from './components/Checkout'
+// import Checkout from './components/Checkout'
+import Success_Screeen from './components/Success_Screeen'
 // import AuthForm from './components/AuthForm'
 // import Cartpage from './components/cartpage'
  
  const page = () => {
    return (
      <div> 
+       <Success_Screeen/>
        {/* <AuthForm/>
         */}
            {/* <Cart_page/> */}
-           <Checkout/>
+           {/* <Checkout/> */}
      </div>
    )
  }
